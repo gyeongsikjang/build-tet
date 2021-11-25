84dd167121607b2dd4a5ce41ce5eb0fed53a1d63
JangGyeongSik <kqkdn1@naver.com>
2021-11-25 16:17:17 +0900

    modify action.yaml

--- gyeongsik-prd/date_update.sh
+++ gyeongsik-prd/date_update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 new_date=`date "+%Y-%m-%d %I:%M:%S %p"`
 old_date=`cat ./values.yaml |grep echo | cut -d \" -f2`
+old_image_tags=`cat ./values.yaml |grep tag: | cut -d \" -f2`
+new_image_tags="green"
+curl v2.wttr.in/Seoul
 sed -i "s/$old_date/$new_date/g" ./values.yaml
+sed -i "s/$old_image_tags/$new_image_tags/g" ./values.yaml 
